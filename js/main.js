39ab94324ea91a25fed6d8869bd728585e4848b0
artchili99 <artur.chilingaryan99@gmail.com>
2025-05-07 18:51:41 +0400

    Allow switching between °C and °F

--- js/main.js
+++ js/main.js
@@ -72,19 +72,43 @@ function updateUI(data) {
          <div id="temp">${main.temp}</div>
         <span id="tempTipe">${tempTipe}</span>
         </div>
-        <button onclick="tempSwitch()" class="btn btn-primary">°F</button>
+        <div id="temps">
+        <button onclick="tempSwitch()" class="btn btn-primary" id="tempSwitch">°F</button>
+        </div>
         <div>${weather[0].description}</div>
         <div>humidity : ${main.humidity}%</div>
         <div>wind speed : ${wind.speed}%</div>
         <img src="http://openweathermap.org/img/wn/${weather[0].icon}.png" alt="${weather[0].description}">
       `;
 }
+function tempSwitchC() {
+    var temp = document.getElementById('temp').innerHTML;
+    temp = (temp - 32) * 5/9;
+    document.getElementById('temp').innerHTML = temp;
+    document.getElementById('tempTipe').innerHTML = "°C";
+    document.getElementById('tempSwitchC').style.display = "none";
+    temps = document.getElementById('temps');
+    temps.innerHTML =
+        `
+       <button onclick="tempSwitch()" class="btn btn-primary" id="tempSwitch">°F</button>
+        `;
 
+
+
+}
 function tempSwitch() {
+
     var temp = document.getElementById('temp').innerHTML;
     temp = (temp * 9/5) + 32;
     document.getElementById('temp').innerHTML = temp;
     document.getElementById('tempTipe').innerHTML = "°F";
+    document.getElementById('tempSwitch').style.display = "none";
+    temps = document.getElementById('temps');
+    temps.innerHTML =
+        `
+        <button onclick="tempSwitchC()" class="btn btn-primary" id="tempSwitchC">°C</button>
+        `;
+
 }
 fadeOut();
 searchBtn.addEventListener('click', submit);
